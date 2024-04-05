efcc17716ffbd0e1aa488e45d57f684efc03ba1c
Bartłomiej Gawęda <bgaweda@abis.krakow.pl>
2024-04-05 21:35:02 +0200

    Test tabler-icon-component for outline icon
    + Configure karma.

---
+++ packages/icons-angular/src/lib/tabler-icon-outline.component.spec.ts
@@ -0,0 +1,87 @@
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import defaultAttributes from '../defaultAttributes';
+import { TablerIcon, TablerIcons } from '../types';
+import { TablerIconComponent } from './tabler-icon.component';
+import { TablerIconModule } from './tabler-icon.module';
+
+describe('TablerIconComponent', () => {
+  let hostComponent: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
+  const getSvgAttr = (attr: string) => fixture.nativeElement.querySelector('svg').getAttribute(attr);
+  let icon: TablerIcon = { name: 'test', type: 'outline', nodes: [['path', { d: 'M8 7h-4', key: 'svg-0' }]] };
+  let icons: TablerIcons = { Test: icon };
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TablerIconComponent, TestHostComponent],
+      imports: [TablerIconModule.pick(icons)]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestHostComponent);
+    hostComponent = fixture.componentInstance;
+  })
+
+  it('should create', () => {
+    fixture.detectChanges();
+    expect(hostComponent).toBeTruthy();
+  });
+
+  it('should have all default values if not provieded', () => {
+    fixture.detectChanges();
+    expect(getSvgAttr('width')).toBe(defaultAttributes.outline.width.toString(10));
+    expect(getSvgAttr('fill')).toBe(defaultAttributes.outline.fill);
+    expect(getSvgAttr('stroke')).toBe(defaultAttributes.outline.stroke);
+    expect(getSvgAttr('stroke-width')).toBe(defaultAttributes.outline['stroke-width'].toString(10));
+    expect(getSvgAttr('stroke-linecap')).toBe(defaultAttributes.outline['stroke-linecap']);
+    expect(getSvgAttr('stroke-linejoin')).toBe(defaultAttributes.outline['stroke-linejoin']);
+  });
+
+  it('should set size', () => {
+    const size = 32;
+    hostComponent.size = size;
+    fixture.detectChanges();
+    expect(getSvgAttr('height')).toBe(size.toString(10));
+    expect(getSvgAttr('width')).toBe(size.toString(10));
+  })
+
+  it('should set color', () => {
+    const color = '#abcabc';
+    hostComponent.color = color;
+    fixture.detectChanges();
+    expect(getSvgAttr('stroke')).toBe(color);
+  })
+
+  it('should set stroke width', () => {
+    const strokeWidth = 1.75;
+    hostComponent.strokeWidth = strokeWidth;
+    fixture.detectChanges();
+    expect(getSvgAttr('stroke-width')).toBe(strokeWidth.toString(10));
+  })
+
+  it('should add all classes', () => {
+    fixture.detectChanges();
+    expect(getSvgAttr('class')).toBe('tabler-icon tabler-icon-test test-class');
+  })
+
+  @Component({
+    selector: 'tabler-test',
+    template: `
+      <tabler-icon
+        [icon]="iconTest"
+        class="test-class"
+        [color]="color"
+        [size]="size"
+        [strokeWidth]="strokeWidth"
+        [class]="class"
+      ></tabler-icon>
+    `,
+  })
+  class TestHostComponent {
+    size?: number;
+    color?: string;
+    strokeWidth?: number;
+    class?: string;
+    iconTest = icon;
+  }
+})
