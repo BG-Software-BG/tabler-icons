9d5fabc15092988bb7ff4539d341f1b0660b8d01
Bartłomiej Gawęda <bgaweda@abis.krakow.pl>
2024-04-06 11:12:50 +0200

    Remove unused IconProps, add karma config in angular.json
    + Remove old module.

--- packages/icons-angular/src/types.ts
+++ packages/icons-angular/src/types.ts
@@ -1,8 +1,3 @@
 export type TablerIconNode = [elementName: string, attrs: Record<string, string>];
 export type TablerIcon = { name: string, type: 'outline' | 'filled', nodes: TablerIconNode[] };
 export type TablerIcons = { [key: string]: TablerIcon };
-
-export interface IconProps extends Partial<Omit<SVGSVGElement, 'stroke'>> {
-  size?: string | number;
-  stroke?: string | number;
-}
